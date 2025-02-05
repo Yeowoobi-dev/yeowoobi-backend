bd895df78579416adc0de81cc69eb85f600e872f
Juuunyeok <junyeok333@gmail.com>
2025-02-05 16:51:08 +0900

    feat: jwt 토큰 검증 코드 추가 #5

--- apps/user/src/auth/service/auth.service.ts
+++ apps/user/src/auth/service/auth.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../../user/user.service';
-import { User } from '../../user/entity/user.entity';
-import { CreateUserDto } from '../../user/dto/create-user.dto';
-import { KakaoAuthService } from './kakao-auth.service';
-import { NotFoundError } from 'rxjs';
 import { AuthUserDto } from '../../common/dto/base-authUser.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
+    private configService: ConfigService
   ) {}
 
 /**
@@ -20,5 +17,12 @@ export class AuthService {
   async generateJwtToken(user: AuthUserDto) {
     const payload = { id: user.id, email: user.email};
     return this.jwtService.sign(payload, { expiresIn: '1h' })
+  }
+
+  /**
+   * jwt 검증
+   */
+  async verifyJwtToken(token: string) {
+    this.jwtService.verify(token, this.configService.get('JWT_SECRET'));
   }
 }
