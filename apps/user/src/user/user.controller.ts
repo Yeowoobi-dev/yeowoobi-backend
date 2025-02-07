d7b0c186189b330a4e32db3eec4760fec29de56b
Juuunyeok <junyeok333@gmail.com>
2025-02-08 00:45:47 +0900

    feat: 유저 닉네임 변경 기능 구현  #7

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Headers, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller()
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  /**
+   * 
+   * @param req 
+   * @param nickname 
+   * @returns 
+   * 변경된 유저 닉네임 반환
+   */
+  @Post('nickname')
+  @UseGuards(AuthGuard('jwt'))
+  async generateNickname(@Req() req, @Body('nickname') nickname: string) {
+    
+    const newNickname =  await this.userService.createNickname(req.user.userId, nickname);
+
+    return { nickname: newNickname }
+  }
 }
--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -3,15 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from '../auth/service/auth.service';
+import { when } from 'joi';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    private readonly authService: AuthService,
   ) {}
   /**
-   * 
+   * socialId 로 사용자 확인 메소드
    * @param socialId 
    * @param socialType 
    * 소셜 아이디와 소셜 타입으로 사용자 확인
@@ -26,7 +29,7 @@ export class UserService {
     return user;
   }
   /**
-   * 
+   * 유저 생성 메소드
    * @param createUserDto 
    * email, name, socialID 로 유저 생성
    * user의 id 는 자동생성 값 사용
@@ -38,5 +41,22 @@ export class UserService {
     const saveUser = await this.userRepository.save(user);
 
     return user;
+  }
+
+  /**
+   * 닉네임 변경 메소드
+   * @param token 
+   * @param nickname 
+   * @returns 
+   * 닉네임이 변경된 사용자의 닉네임
+   */
+  async createNickname(userId: string, nickname: string) {
+    const user = await this.userRepository.findOne({
+      where: { id: userId }
+    })
+    user.nickname = nickname;
+    const saveUser = await this.userRepository.save(user);
+    
+    return saveUser.nickname;
   }
 }
