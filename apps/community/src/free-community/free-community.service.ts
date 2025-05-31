ffbe4398646ba4b0bd6011478faeed257fafe1c9
Juuunyeok <junyeok333@gmail.com>
2025-05-31 12:34:40 +0900

    fix: 댓글 목록 response에 좋아요 개수  칼럼 추가

--- apps/community/src/free-community/free-community.service.ts
+++ apps/community/src/free-community/free-community.service.ts
@@ -298,6 +298,7 @@ export class FreeCommunityService {
         updatedAt: comment.updatedAt,
         parentId: comment.parentId,
         level: comment.level,
+        likesCount: comment.likesCount,
         children: [],
       };
   
